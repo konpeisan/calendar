09536f4996d7b37717142635e4579d2d11a85504
konpeisan <introjection.19@icloud.com>
2021-06-12 14:19:05 +0900

    カレンダーの見た目の変更

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -2,4 +2,17 @@ class BudgetsController < ApplicationController
   def index
     @budgets = Budget.all
   end
+
+  def new
+    @budget = Budget.new
+  end
+
+  def create
+    Budget.create(budget_params)
+  end
+
+  private
+  def budget_params
+    params.require(:budget).permit(:start_time,:price,:recovery,:yen_id,:memo)
+  end
 end
