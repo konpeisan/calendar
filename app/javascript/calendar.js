ab9848fdc35594359f595402cb7629442dfe1bea
konpeisan <introjection.19@icloud.com>
2021-06-18 19:42:29 +0900

    カレンダーのマス目選択機能

--- app/javascript/calendar.js
+++ app/javascript/calendar.js
@@ -1,25 +1,21 @@
 function pullDown(){
-  const pullDownButton = document.getElementById("calendar-button")
+  const table = document.getElementById("calendar-button")
   const pullDownParents = document.getElementById("pull-down")
+  const Td = table.getElementsByTagName('td')
 
-  pullDownButton.addEventListener('mouseover', function(){
-    pullDownButton.setAttribute("style", "background-color:blue;")
-  })
-  pullDownButton.addEventListener('mouseout', function(){
-    pullDownButton.removeAttribute("style", "background-color:blue;")
-  })
-  pullDownButton.addEventListener('click', function(e){
-    if (pullDownParents.getAttribute("type") == "test"){
-      console.log("click")
-      pullDownParents.removeAttribute("style", "display:block;")
-      pullDownParents.removeAttribute("type", "test")
-    } else {
-      pullDownParents.setAttribute("style", "display:block;")
-      pullDownParents.style.left = e.pageX+"px"
-      pullDownParents.style.top = e.pageY+"px"
-      pullDownParents.setAttribute("type", "test")
+  for(var i=0; i<Td.length; i++){
+    Td[i].onclick= function(e){
+      if (pullDownParents.getAttribute("type") == "test"){
+        pullDownParents.removeAttribute("style", "display:block;")
+        pullDownParents.removeAttribute("type", "test")
+      } else {
+        pullDownParents.setAttribute("style", "display:block;")
+        pullDownParents.style.left = e.pageX+"px"
+        pullDownParents.style.top = e.pageY+"px"
+        pullDownParents.setAttribute("type", "test")
+      }
     }
-  })
+  }
 }
 
 window.addEventListener('load', pullDown)
