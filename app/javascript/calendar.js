fc15f854286a7f12b4d6445fec5a16cb50677838
konpeisan <introjection.19@icloud.com>
2021-06-19 16:12:34 +0900

    mouseover時のサブメニュー表示機能

--- app/javascript/calendar.js
+++ app/javascript/calendar.js
@@ -1,21 +1,22 @@
-function pullDown(){
-  const table = document.getElementById("calendar-button")
-  const pullDownParents = document.getElementById("pull-down")
-  const Td = table.getElementsByTagName('td')
+//function pullDown(){
+//  let table = document.getElementById("calendar-button")
+//  let pullDownParents = document.getElementById("pull-down")
+//  let cells = table.querySelectorAll('td')
 
-  for(var i=0; i<Td.length; i++){
-    Td[i].onclick= function(e){
-      if (pullDownParents.getAttribute("type") == "test"){
-        pullDownParents.removeAttribute("style", "display:block;")
-        pullDownParents.removeAttribute("type", "test")
-      } else {
-        pullDownParents.setAttribute("style", "display:block;")
-        pullDownParents.style.left = e.pageX+"px"
-        pullDownParents.style.top = e.pageY+"px"
-        pullDownParents.setAttribute("type", "test")
-      }
-    }
-  }
-}
+//  cells.forEach(function (cell){
+//    cell.addEventListener('click', function(e){
+//      if (pullDownParents.getAttribute("type") == "test"){
+//        pullDownParents.removeAttribute("style", "display:block;")
+//       pullDownParents.removeAttribute("type", "test")
+//      } else {
+//        pullDownParents.setAttribute("style", "display:block;")
+//        pullDownParents.style.left = e.pageX+"px"
+//        pullDownParents.style.top = e.pageY+"px"
+//        pullDownParents.setAttribute("type", "test")
+//        console.log(cell.innerHTML)
+//      }
+//    })
+//  })
+//}
 
-window.addEventListener('load', pullDown)
+//window.addEventListener('load', pullDown)
