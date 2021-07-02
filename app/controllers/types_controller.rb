5563080e13fa5a39290fdb4509a77437301c7315
konpeisan <introjection.19@icloud.com>
2021-07-02 19:33:46 +0900

    機種名登録ページの実装

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,2 +1,44 @@
 class TypesController < ApplicationController
+  before_action :set_type, only: [:edit, :update, :destroy]
+
+  def index
+    @type = Type.new
+    @types = Type.all
+  end
+
+  def create
+    @type = Type.new(type_params)
+    if @type.save
+      redirect_to types_path
+    else
+      @types = Type.all
+      render :index
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @type.update(type_params)
+      redirect_to types_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @type.destroy
+    redirect_to types_path
+  end
+
+  private
+
+  def set_type
+    @type = Type.find(params[:id])
+  end
+
+  def type_params
+    params.require(:type).permit(:type_name,:pachinko,:slot)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
     end
   end
   resources :halls, except: [:new, :show]
+  resources :types, except: [:new, :show]
 end
