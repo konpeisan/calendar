5269925696eed4a87f040fe0642fb03cfef9cce1
konpeisan <introjection.19@icloud.com>
2021-08-05 21:55:49 +0900

    ボタン編集

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,6 +1,4 @@
 class Budget < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :yen
   belongs_to :hall, optional: true
   belongs_to :type, optional: true
   has_one_attached :image
