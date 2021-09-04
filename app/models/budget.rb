a86296f77bf220dd09bb34bc5b2a047665c9bd60
konpeisan <introjection.19@icloud.com>
2021-09-04 23:29:22 +0900

    validateの追加

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,4 +1,7 @@
 class Budget < ApplicationRecord
   belongs_to :hall, optional: true
   belongs_to :type, optional: true
+
+  validates :price, numericality: true
+  validates :recovery, numericality: true
 end
