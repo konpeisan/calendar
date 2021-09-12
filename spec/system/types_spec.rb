426dee65d437a203480d6b7f463fdde20c38a1eb
konpeisan <introjection.19@icloud.com>
2021-09-12 20:24:45 +0900

    type結合テスト追加

--- spec/system/types_spec.rb
+++ spec/system/types_spec.rb
@@ -30,3 +30,66 @@ RSpec.describe "機種登録", type: :system do
   end
 end
 
+RSpec.describe '機種の編集', type: :system do
+
+  before do
+    @type1 = FactoryBot.create(:type)
+    @type2 = FactoryBot.create(:type)
+  end
+
+  context '機種の編集ができるとき' do
+    it 'ログインしたユーザーは自分が登録した機種の編集ができる' do
+      # type1を投稿したユーザーでログインする
+      sign_in(@type1.user)
+      # 機種ページへ遷移する
+      visit types_path
+      # type1に「編集」へのリンクがあることを確認する
+      expect(page).to have_link '編集', href: edit_type_path(@type1)
+      # 編集ページへ遷移する
+      visit edit_type_path(@type1)
+      # すでに投稿済みの内容がフォームに入っていることを確認する
+      expect(find('#type_type_name').value).to eq(@type1.type_name)
+      expect(page).to have_checked_field with: "#{@type1.pachi_slot}", visible: false
+      expect(page).to have_checked_field with: "#{@type1.kind}", visible: false
+      # 投稿内容を編集する
+      expect(page).to have_checked_field with: 'パチンコ', visible: false
+      find('label[for=type_kind_ミドル]').click
+      expect(page).to have_checked_field with: 'ミドル', visible: false
+      fill_in '機種名', with: "#{@type1.type_name}+編集した機種名"
+      # 編集してもTweetモデルのカウントは変わらないことを確認する
+      expect{find('input[name="commit"]').click}.to change { Type.count }.by(0)
+      # 機種ページに遷移したことを確認する
+      expect(current_path).to eq types_path
+      # 機種ページには先ほど変更した内容の機種名が存在することを確認する
+      expect(page).to have_content("#{@type1.type_name}+編集した機種名")
+    end
+  end
+end
+
+RSpec.describe 'ツイート削除', type: :system do
+
+  before do
+    @type1 = FactoryBot.create(:type)
+    @type2 = FactoryBot.create(:type)
+  end
+
+  context '機種を削除ができるとき' do
+    it 'ログインしたユーザーは自らが登録した機種の削除ができる' do
+      # type1を投稿したユーザーでログインする
+      sign_in(@type1.user)
+      # 機種ページへ遷移する
+      visit types_path
+      # type1に「削除」へのリンクがあることを確認する
+      expect(page).to have_link '削除', href: type_path(@type1)
+      #type1削除へのリンクをクリックする
+      click_on '削除'
+      #削除ボタンを押すとダイアログが出てくる
+      page.driver.browser.switch_to.alert.accept
+      # 機種ページに遷移したことを確認する
+      expect(current_path).to eq types_path
+      # 機種ページにはtype1の内容が存在しないことを確認する
+      expect(page).to have_no_content("#{@type1.type_name}")
+    end
+  end
+end
+
